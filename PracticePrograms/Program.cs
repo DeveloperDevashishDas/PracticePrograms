9a26db195457bb329f8fe7241f59a3b217a7c613
Devashish <devashishdas508@gmail.com>
2024-05-19 12:32:35 +0530

    Calculator program pushed.

---
+++ PracticePrograms/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PracticePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            do
+            {
+                int a, b, result;
+                Console.WriteLine("1st Number");
+                a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("2nd Number");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Operation + - / *");
+                string operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "+":
+                        result = a + b;
+                        Console.WriteLine("{0}", result);
+                        break;
+
+                    case "-":
+                        result = a - b;
+                        Console.WriteLine("{0}", result);
+                        break;
+
+                    case "*":
+                        result = a * b;
+                        Console.WriteLine("{0}", result);
+                        break;
+
+                    case "/":
+                        result = a / b;
+                        Console.WriteLine("{0}", result);
+                        break;
+
+                }
+                Console.ReadLine();
+                Console.Write("Do you want to continue(y/n):");
+                value = Console.ReadLine();
+            }
+            while (value == "y" || value == "Y");
+
+        }
+
+    }
+}
