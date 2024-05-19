092702fadc3ea709950f43ee7bd31755b2a29ae3
Devashish <devashishdas508@gmail.com>
2024-05-19 12:53:33 +0530

    Program of Check If Two String Arrays are Equivalent added.

---
+++ PracticePrograms/TwoStringArraysareEquivalent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticePrograms
+{
+    internal class TwoStringArraysareEquivalent
+    {
+        // Check If Two String Arrays are Equivalent
+        // word1 = ["ab", "c"], word2 = ["a", "bc"]
+        public static string Compare(string[] word)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in word)
+            {
+                sb.Append(item);
+            }
+            return sb.ToString();
+        }
+
+        static void Main()
+        {
+            string[] word1 = new string[2] { "ab", "c" };
+            string[] word2 = new string[2] { "a", "bc" };
+
+            string s1 = TwoStringArraysareEquivalent.Compare(word1);
+            string s2 = TwoStringArraysareEquivalent.Compare(word2);
+            Console.WriteLine("{0}", s1.Equals(s2));
+            Console.ReadLine();
+        }
+    }
+}
