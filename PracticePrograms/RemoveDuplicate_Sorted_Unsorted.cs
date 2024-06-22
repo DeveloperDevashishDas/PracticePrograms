4e2c918c173827003f8f0636e2e18c7c3a64a6f6
Devashish <devashishdas508@gmail.com>
2024-06-22 11:38:26 +0530

    Remove Duplicate Sorted Unsorted

---
+++ PracticePrograms/RemoveDuplicate_Sorted_Unsorted.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticePrograms
+{
+    public class RemoveDuplicate_Sorted_Unsorted
+    {
+        static void Main()
+        {
+            //Sorted List Unsorted
+            int[] a = { 5, 5, 4, 3, 3, 2 };
+            HashSet<int> hs = new HashSet<int>();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                hs.Add(a[i]);
+            }
+
+            foreach (var item in hs)
+            {
+                Console.WriteLine("{0}", item);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
