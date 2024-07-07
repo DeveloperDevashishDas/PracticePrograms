a3d85489508843d2c97f653f3efd5e5ba8d80fa4
Devashish <devashishdas508@gmail.com>
2024-07-07 11:14:37 +0530

    Program of Remove Duplicate from Sorted added.

---
+++ PracticePrograms/RemoveDuplicatefromSorted.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticePrograms
+{
+    public class RemoveDuplicatefromSorted
+    {
+        static void Main()
+        {
+            //Sorted List
+            int[] a = { 1, 2, 2, 3, 3 };
+            int j = 0;
+            int[] temp = new int[a.Length];
+
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i] != a[i + 1])
+                {
+                    temp[j] = a[i];
+                    j++;
+                }
+            }
+            temp[j] = a[a.Length - 1];
+            foreach (var item in temp)
+            {
+                Console.WriteLine("{0}", item);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
