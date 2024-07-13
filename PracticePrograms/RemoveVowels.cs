0ff078842da4aa41b327650d8c68049038cf1ce9
Devashish <devashishdas508@gmail.com>
2024-07-13 19:45:56 +0530

    Program to remove vowels from a string.

---
+++ PracticePrograms/RemoveVowels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticePrograms
+{
+    public class RemoveVowels
+    {
+        static void Main()
+        {
+            string input = "Hello Codeauri Family. enter a string here to remove 	    all Vowels:";
+            string vowels = "aeiouAEIOU";
+            string output = "";
+            foreach (char item in input)
+            {
+                if (!vowels.Contains(item))
+                {
+                    output += item;
+                }
+            }
+            Console.WriteLine("{0}", output);
+            Console.ReadLine();
+        }
+    }
+}
