d30bfcb77cf254edf138fdd141186b7d61c3f5c3
Devashish <devashishdas508@gmail.com>
2026-03-19 20:07:23 +0530

    Reverse String Progrma added.

---
+++ PracticePrograms/ReverseString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticePrograms
+{
+    public class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter string to be reversed");
+            string input = Console.ReadLine();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = input.Length - 1; i >=0  ; i--)
+            {
+                stringBuilder.Append(input[i]);
+            }    
+            Console.WriteLine("INPUT FROM USER {0}", input);
+            Console.WriteLine("REVERSED STRING IS {0}", stringBuilder);
+            Console.ReadLine();
+        }
+    }
+}
