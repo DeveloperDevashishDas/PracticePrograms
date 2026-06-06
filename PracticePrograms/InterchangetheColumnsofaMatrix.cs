4d226dccf7bd42963e577b245d83a653eca72411
Devashish <devashishdas508@gmail.com>
2026-06-06 11:52:12 +0530

    Interchange the Columns of a Matrix added.

---
+++ PracticePrograms/InterchangetheColumnsofaMatrix.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticePrograms
+{
+    public class InterchangetheColumnsofaMatrix
+    {
+        static void Main()
+        {
+            int rows, cols;
+
+            Console.Write("Enter number of rows: ");
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter number of columns: ");
+            cols = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[rows, cols];
+
+            Console.WriteLine("Enter matrix elements:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.Write("Enter first column number to swap (0-based index): ");
+            int col1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second column number to swap (0-based index): ");
+            int col2 = Convert.ToInt32(Console.ReadLine());
+
+            // Swap the columns
+            for (int i = 0; i < rows; i++)
+            {
+                int temp = matrix[i, col1];
+                matrix[i, col1] = matrix[i, col2];
+                matrix[i, col2] = temp;
+            }
+
+            Console.WriteLine("\nMatrix after interchanging columns:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
