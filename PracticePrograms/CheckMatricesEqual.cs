f77d0f79b79229b410f3f732d110e65d4cb42ea2
Devashish <devashishdas508@gmail.com>
2026-08-23 19:04:55 +0530

    Unwanted code removed.

--- PracticePrograms/CheckMatricesEqual.cs
+++ PracticePrograms/CheckMatricesEqual.cs
@@ -15,9 +15,6 @@ namespace PracticePrograms
             Console.Write("Enter number of rows: ");
             rows = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter number of rows: ");
-            rows = Convert.ToInt32(Console.ReadLine());
-
             Console.Write("Enter number of columns: ");
             cols = Convert.ToInt32(Console.ReadLine());
 
